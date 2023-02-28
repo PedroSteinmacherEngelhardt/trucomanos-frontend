1b03c990b535a7e1bf1ee8591b24393af8993b3a
GUSTAVO SATIG <gustavo.satig@catolicasc.edu.br>
2023-02-27 21:29:55 -0300

    finalizacao about e home

--- trucomanos/src/app/app.module.ts
+++ trucomanos/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app-component/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
